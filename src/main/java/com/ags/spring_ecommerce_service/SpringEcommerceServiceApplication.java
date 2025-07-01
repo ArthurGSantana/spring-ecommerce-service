7ba651debb45eebde4e442b3b5c20cc863b758ca
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-01 08:31:20 -0300

    initialize spring-ecommerce-service project with Gradle setup and basic application structure

---
+++ src/main/java/com/ags/spring_ecommerce_service/SpringEcommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ags.spring_ecommerce_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ags/spring_ecommerce_service/SpringEcommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ags.spring_ecommerce_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
