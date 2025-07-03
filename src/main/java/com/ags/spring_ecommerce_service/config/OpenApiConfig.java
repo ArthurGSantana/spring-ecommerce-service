ec4f373b592a79106cbe40701c5fee57e78bad0c
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-03 11:12:22 -0300

    add OpenAPI configuration for API documentation and security

---
+++ src/main/java/com/ags/spring_ecommerce_service/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.ags.spring_ecommerce_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "E-commerce Service API",
+                description = "API para gerenciamento de e-commerce",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Arthur G. Santana",
+                        email = "seu-email@exemplo.com"
+                )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
