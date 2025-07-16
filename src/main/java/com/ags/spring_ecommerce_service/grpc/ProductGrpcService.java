cca5cd5c0534c91fa41ac1993a573347160be18e
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-16 10:57:19 -0300

    implement ProductGrpcService with createProduct RPC and update proto to include stock, sku, and status fields

---
+++ src/main/java/com/ags/spring_ecommerce_service/grpc/ProductGrpcService.java
@@ -0,0 +1,53 @@
+package com.ags.spring_ecommerce_service.grpc;
+
+import com.ags.spring_ecommerce_service.dto.ProductDto;
+import com.ags.spring_ecommerce_service.enums.ProductStatusEnum;
+import com.ags.spring_ecommerce_service.service.ProductService;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+@RequiredArgsConstructor
+public class ProductGrpcService extends ProductServiceGrpc.ProductServiceImplBase {
+  private final ProductService productService;
+
+  @Override
+  public void createProduct(
+      CreateProductRequest request, StreamObserver<CreateProductResponse> responseObserver) {
+    try {
+      var productDto =
+          ProductDto.builder()
+              .name(request.getName())
+              .description(request.getDescription())
+              .price(BigDecimal.valueOf(request.getPrice()))
+              .stock(request.getStock())
+              .status(ProductStatusEnum.AVAILABLE)
+              .build();
+
+      var product = productService.createProduct(productDto);
+
+      var response =
+          CreateProductResponse.newBuilder()
+              .setId(product.getId())
+              .setSku(product.getSku())
+              .setName(product.getName())
+              .setDescription(product.getDescription())
+              .setPrice(product.getPrice().doubleValue())
+              .setStock(product.getStock())
+              .setStatus(product.getStatus().name())
+              .build();
+
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+
+    } catch (Exception e) {
+      responseObserver.onError(
+          Status.INTERNAL
+              .withDescription("Create Product Error: " + e.getMessage())
+              .asRuntimeException());
+    }
+  }
+}
