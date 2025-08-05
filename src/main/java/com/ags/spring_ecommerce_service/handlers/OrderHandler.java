1334f496822f1f47ba724acddf7056ff7dce2cdc
Arthur Santana <arthurgn.santana@gmail.com>
2025-08-05 10:00:22 -0300

    enhance OrderHandler; add message key logging and operation type handling in order processing

--- src/main/java/com/ags/spring_ecommerce_service/handlers/OrderHandler.java
+++ src/main/java/com/ags/spring_ecommerce_service/handlers/OrderHandler.java
@@ -3,9 +3,11 @@ package com.ags.spring_ecommerce_service.handlers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,8 +17,17 @@ public class OrderHandler {
   private final ObjectMapper objectMapper;
 
   @KafkaListener(topics = "${spring.kafka.topics.order-handler}", groupId = "order-handler-group")
-  public void handleOrder(String message) throws JsonProcessingException {
+  public void handleOrder(@Nullable String message, @Header("kafka_receivedMessageKey") String keyJson) throws JsonProcessingException {
     EventData<OrderData> eventData = objectMapper.readValue(message, new TypeReference<>() {});
+
+    log.info("Received key from Kafka: {}", keyJson);
+
+    switch (eventData.getOp()) {
+      case "c" -> log.info("Operation is create");
+      case "u" -> log.info("Operation is update");
+      case "d" -> log.info("Operation is delete");
+      default -> log.warn("Unknown operation: {}", eventData.getOp());
+    }
 
     OrderData order = eventData.getBefore();
     OrderData after = eventData.getAfter();
