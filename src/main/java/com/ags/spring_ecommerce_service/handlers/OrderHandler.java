e5f17694ff4f37f273a69cc6024f9ab8bea281d0
Arthur Santana <arthurgn.santana@gmail.com>
2025-08-05 09:23:06 -0300

    enhance OrderHandler; parse order messages into EventData and log received messages from Debezium

--- src/main/java/com/ags/spring_ecommerce_service/handlers/OrderHandler.java
+++ src/main/java/com/ags/spring_ecommerce_service/handlers/OrderHandler.java
@@ -1,5 +1,8 @@
 package com.ags.spring_ecommerce_service.handlers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,11 +12,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class OrderHandler {
+  private final ObjectMapper objectMapper;
 
   @KafkaListener(topics = "${spring.kafka.topics.order-handler}", groupId = "order-handler-group")
-  public void handleOrder(String message) {
-    log.info("Received order message: {}", message);
-    // Here you would typically process the order message, e.g., save it to the database
-    // or perform some business logic.
+  public void handleOrder(String message) throws JsonProcessingException {
+    EventData<OrderData> eventData = objectMapper.readValue(message, new TypeReference<>() {
+    });
+
+    OrderData order = eventData.getBefore();
+    OrderData after = eventData.getAfter();
+
+    log.info("Received order message from Debezium: {}", message);
   }
 }
