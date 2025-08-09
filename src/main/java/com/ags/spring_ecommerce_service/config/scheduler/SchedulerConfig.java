17cc3ff141e45224c35b1518450f76021bef03b5
Arthur Santana <arthurgn.santana@gmail.com>
2025-08-09 11:25:10 -0300

    add OrderStatusUpdateJob and SchedulerConfig; implement scheduled job for order status updates with ShedLock integration

---
+++ src/main/java/com/ags/spring_ecommerce_service/config/scheduler/SchedulerConfig.java
@@ -0,0 +1,21 @@
+package com.ags.spring_ecommerce_service.config.scheduler;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+public class SchedulerConfig {
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
+    }
+}
